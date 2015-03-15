26363767aa8b2b03f64cb2e42418cccf240c8c79
Bryan Knouse <bryan.knouse@gmail.com>
2015-03-15 10:09:30 -0400

    index, data sections up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,27 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+
+
+	// Smooth link scroll
+	/////////////////////////////////////////////////////////////////////////////////////////////
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+	        || location.hostname == this.hostname) {
+
+	        var target = $(this.hash);
+	        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	           if (target.length) {
+	             $('html,body').animate({
+	                 scrollTop: target.offset().top + 5
+	            }, 750);
+	            return false;
+	        }
+	    }
+	});
+
+
+});
